2705eabb4a707f05ebc9c0fc2bcfcfc9fcd4ac6a
unknown <george.calderon2000@gmail.com>
2025-06-24 15:29:22 -0400

    adjusted json structure of data - nested relative values based on relationships

--- backend/server.js
+++ backend/server.js
@@ -2,32 +2,61 @@ const express = require('express')
 const cors = require("cors");
 const { PrismaClient } = require('./generated/prisma');
 
-const prisma = new PrismaClient(); //new prisma client
-const app = express(); //new express client
+const prisma = new PrismaClient();
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+// app.get("/api/scienceFair", async (request, response) => {
+//     try {
+        
+//         const teamData = await prisma.team.findMany();
+//         const scientistData = await prisma.scientist.findMany();
+//         const reviewData = await prisma.review.findMany();
+
+//         const data = { 
+//             scientists: scientistData,
+//             teams: teamData,
+//             reviews: reviewData
+//         };
+
+//         return response.json(data);
+
+//     } catch (error) {
+//         console.error ('Error fetching data: ', error);
+//         response.status(500).json({error: 'Internal server error'});
+//     }
+// });
+
 app.get("/api/scienceFair", async (request, response) => {
     try {
         
-        const teamData = await prisma.team.findMany();
-        const scientistData = await prisma.scientist.findMany();
-        const reviewData = await prisma.review.findMany();
+        /*
+        we're grabbing data from the DB in the shape of the structure that 
+        follows their relationship
+        */
 
-        const data = { 
-            scientists: scientistData,
-            teams: teamData,
-            reviews: reviewData
-        };
+        const teamFullOverview = await prisma.team.findMany({
+            include: { 
+                scientists: { 
+                    include: { 
+                        reviews: true 
+                    }
+                }
+            }
+        });
 
-        return response.json(data);
+        return response.json(teamFullOverview);
 
     } catch (error) {
-        console.error ('Error fetching data: ', error);
-        response.status(500).json({error: 'Internal server error'});
+        console.error(`Error fetching data: ${error}`);
+        response.status(500).json({
+            error: 'Internal server Error'
+        });
     }
 });
+
 
 app.listen(5555, () => {
     console.log("Server is listening on port 5555");
