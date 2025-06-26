31bcf6b6811b4f97b624e90a0f180d12866666c6
unknown <george.calderon2000@gmail.com>
2025-06-26 15:50:08 -0400

    reworked json info containers for easier css formatting - add divs for info for reviews and scientists - still need to finish cards

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,5 @@
 const rosterContainer = document.getElementById("teamsRoster");
+const scientistsContainer = document.getElementById("scientistsInTeam")
 const reviewsContainer = document.getElementById("reviewsForScientists");
 const documentBody = document.getElementById("body");
 const errorBody = document.getElementById("error");
@@ -37,6 +38,7 @@ async function fetchAndDisplayData () {
         <ul>
           <li><a href="#" class="category">Home</a></li>
           <li><a href="#teams" class="category">Teams</a></li>
+          <li><a href="#scientists" class="category">Scientists</a></li>
           <li><a href="#reviews" class="category">Reviews</a></li>
         </ul>
       </nav>
@@ -51,73 +53,125 @@ async function fetchAndDisplayData () {
     </div>
     `;
 
-    const roster = `
-      <ul class="rosterInfo">
-        <div id="rosterInfoScrollable">
-          ${data.map(roster => `
+    // const roster = `
+    //   <ul class="rosterInfo">
+    //     <div id="rosterInfoScrollable">
+    //       ${data.map(roster => `
 
-              <li>
-                <div id="rosterCard">
-                  <h2 id="teamHeader">Faction ${roster.id} - ${roster.teamName}</h2>
-                  <strong class="teamInfo">Invention: </strong> <p>${roster.invention}</p> <br>
-                  <strong class="teamInfo">Description: </strong> <p>${roster.description}</p> <br>
-                  <strong class="teamInfo">Victory Theme: </strong> <p>${roster.themeSong}</p> <br>
-                </div>
-                ${roster.scientists.map(scientistInfo => `
+    //           <li>
+    //             <div id="rosterCard">
+    //               <h2 id="teamHeader">Faction ${roster.id} - ${roster.teamName}</h2>
+    //               <strong class="teamInfo">Invention: </strong> <p>${roster.invention}</p> <br>
+    //               <strong class="teamInfo">Description: </strong> <p>${roster.description}</p> <br>
+    //               <strong class="teamInfo">Victory Theme: </strong> <p>${roster.themeSong}</p> <br>
+    //             </div>
+    //             ${roster.scientists.map(scientistInfo => `
                   
-                <ul class="scientistInfo">
+    //             <ul class="scientistInfo">
 
-                  <li>
-                    <div id="scientistCard">
-                      <strong class="teamInfo">${scientistInfo.role}: </strong> <p>${scientistInfo.name}</p> <br>
-                      <strong class="teamInfo">Geographic Origin: </strong> <p>${scientistInfo.location}</p> <br>
-                      <strong class="teamInfo">Era: </strong> <p>${scientistInfo.era}</p> <br>
-                      <strong class="teamInfo">Victory Emoji: </strong> <p>${scientistInfo.favEmoji}</p> <br>
-                      <strong class="teamInfo">Signup Number: </strong> <p>${scientistInfo.id}</p> <br>
-                    </div>
-                  </li>
+    //               <li>
+    //                 <div id="scientistCard">
+    //                   <strong class="teamInfo">${scientistInfo.role}: </strong> <p>${scientistInfo.name}</p> <br>
+    //                   <strong class="teamInfo">Geographic Origin: </strong> <p>${scientistInfo.location}</p> <br>
+    //                   <strong class="teamInfo">Era: </strong> <p>${scientistInfo.era}</p> <br>
+    //                   <strong class="teamInfo">Victory Emoji: </strong> <p>${scientistInfo.favEmoji}</p> <br>
+    //                   <strong class="teamInfo">Signup Number: </strong> <p>${scientistInfo.id}</p> <br>
+    //                 </div>
+    //               </li>
 
-                </ul>
+    //             </ul>
 
-              </li>
+    //           </li>
               
+    //             `).join(" ")}
+
+    //           </li>
+              
+    //           `).join(" ")}
+    //     </div>
+    //   </ul>
+    // `;
+
+    const roster = `
+      <div class="rosterContainer">
+        ${data.map(roster => `
+          <div class="rosterCard">
+            <h2 class="teamHeader">Faction ${roster.id} - ${roster.teamName}</h2>
+              <div class="teamDetails">
+                <strong class="infoHeader">Invention: </strong><p>${roster.invention}</p>
+                <strong class="infoHeader">Description: </strong><p>${roster.description}</p>
+                <strong class="infoHeader">Victory Theme: </strong><p>${roster.themeSong}</p>
+              </div>
+          </div>
+          `).join(" ")}
+      </div>
+    `;
+
+    const scientist = `
+      <div class="scientistsContainer">
+        ${data.map(data => `
+          <div class="scientistCard">
+            <h2>${data.teamName}' faction members: </h2>
+              ${data.scientists.map(scientists => `
+                <strong class="infoHeader">${scientists.role}: </strong><p>${scientists.name}</p>
+                <strong class="infoHeader">Era: </strong><p>${scientists.era}</p>
+                <strong class="infoHeader">Location: </strong><p>${scientists.location}</p>
+                <strong class="infoHeader">Victory Emote: </strong><p>${scientists.favEmoji}</p>                
                 `).join(" ")}
-
-              </li>
-              
-              `).join(" ")}
-        </div>
-      </ul>
+          </div>
+          `).join(" ")}
+      </div>
     `;
 
     const reviews = `
-      <ul id="reviewsInfo">
-        <div id="reviewsInfoScrollable">
-          ${data.map(teams => teams.scientists.map(scientists => `
-
-        <li>
-          <div id="reviewCard">
-            <h2>Reviews for ${scientists.name}</h2><p>by ${scientists.reviews.map(reviews => " " + reviews.reviewerName)}</p> <br>
+      <div class="reviewsContainer">
+        ${data.map(data => data.scientists.map(data => `
+          <div class="reviewCard">
+            <h2>Reviews for ${data.name}</h2>
+            <div class="reviewCard_reviewerInfo">
+              ${data.reviews.map(review => `
+                <strong class="infoHeader">Reviewer Name: </strong><p>${review.reviewerName}</p>
+                <strong class="infoHeader">Verified Association Member: </strong><p>${review.fromMadSci}</p>
+                <strong class="infoHeader">Rating: </strong><p>${review.ratingStars}</p>
+                <strong class="infoHeader">Review: </strong><p>${review.reviewDesc}</p>
+                <strong class="infoHeader">Audience Seat Number: </strong><p>${review.id}</p>
+                `).join(" ")}
+            </div>
           </div>
-          ${scientists.reviews.map(reviews => `
-          <div id="reviewCardDetails">
-            <strong class="teamInfo">Audience Seat Number: </strong> <p>${reviews.id}</p> <br>
-            <strong class="teamInfo">Rating: </strong> <p>${reviews.ratingStars} Stars</p> <br>
-            <strong class="teamInfo">Reviewer Name: </strong> <p>${reviews.reviewerName}</p> <br>
-            <strong class="teamInfo">Mad-Scientist Foundation Member? </strong> <p>${reviews.fromMadSci}</p> <br>
-            <strong class="teamInfo">Review: </strong> <p>${reviews.reviewDesc}</p> <br>
-            
-            `).join(" ")}
-          </div>  
-        </li>
 
-        `).join(" ")).join(" ")}
-        </div>
-      <ul>
-    `;
+          `).join(" ")).join(" ")}
+      </div>
+    `
+
+    // const reviews = `
+    //   <ul id="reviewsInfo">
+    //     <div id="reviewsInfoScrollable">
+    //       ${data.map(teams => teams.scientists.map(scientists => `
+
+    //     <li>
+    //       <div id="reviewCard">
+    //         <h2>Reviews for ${scientists.name}</h2><p>by ${scientists.reviews.map(reviews => " " + reviews.reviewerName)}</p> <br>
+    //       </div>
+    //       ${scientists.reviews.map(reviews => `
+    //       <div id="reviewCardDetails">
+    //         <strong class="teamInfo">Audience Seat Number: </strong> <p>${reviews.id}</p> <br>
+    //         <strong class="teamInfo">Rating: </strong> <p>${reviews.ratingStars} Stars</p> <br>
+    //         <strong class="teamInfo">Reviewer Name: </strong> <p>${reviews.reviewerName}</p> <br>
+    //         <strong class="teamInfo">Mad-Scientist Foundation Member? </strong> <p>${reviews.fromMadSci}</p> <br>
+    //         <strong class="teamInfo">Review: </strong> <p>${reviews.reviewDesc}</p> <br>
+            
+    //         `).join(" ")}
+    //       </div>  
+    //     </li>
+
+    //     `).join(" ")).join(" ")}
+    //     </div>
+    //   <ul>
+    // `;
     
     documentBody.innerHTML = body;
     rosterContainer.innerHTML = roster;
+    scientistsContainer.innerHTML = scientist;
     reviewsContainer.innerHTML = reviews;
 
   } catch (error) {
